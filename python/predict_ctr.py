43c26ddd0b97f01507c026d42b8061951640b78f
zjgtan <zjgtan@126.com>
2024-06-18 14:07:07 +0800

    u

--- python/predict_ctr.py
+++ python/predict_ctr.py
@@ -16,7 +16,7 @@ def parse_example(record):
 def train_epoch(model, dataset, optimizer):
     for batch in tqdm(dataset):
         with tf.GradientTape() as tape:
-            output_logits = model(batch)
+            output_logits = model(batch["feat"])
             loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch["click"], logits=output_logits))
             click_auc = roc_auc_score(batch["click"], tf.nn.sigmoid(output_logits[0]))
 
@@ -30,7 +30,7 @@ def eval(model, dataset):
     click_logits_list, click_list = [], []
 
     for batch in tqdm(dataset):
-        output_logits = model(batch)
+        output_logits = model(batch["feat"])
         click_logits_list.append(output_logits)
         click_list.append(batch["click"])
 
