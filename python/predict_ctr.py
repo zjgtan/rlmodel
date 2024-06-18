1ba71630477c4948aed0cddf696fe466aaaf1276
zjgtan <zjgtan@126.com>
2024-06-18 14:00:45 +0800

    u

--- python/predict_ctr.py
+++ python/predict_ctr.py
@@ -4,6 +4,7 @@ import numpy as np
 from lr import LR
 import tensorflow as tf
 import tqdm
+import sys
 
 def parse_example(record):
     schema = {}
@@ -24,17 +25,32 @@ def train_epoch(model, dataset, optimizer):
 
         print(loss, click_auc)
 
-        idx += 1
+
+def eval(model, dataset):
+    click_logits_list, click_list = [], []
+
+    for batch in tqdm(dataset):
+        output_logits = model(batch)
+        click_logits_list.append(output_logits)
+        click_list.append(batch["click"])
+
+
+    all_click_logits = tf.concat(click_logits_list, axis=0)
+    all_click = tf.concat(click_list, axis=0)
+
+    click_auc = roc_auc_score(all_click, tf.nn.sigmoid(all_click_logits))
+
+    print("test: ", click_auc)
 
 if __name__ == "__main__":
+    camp = sys.argv[1]
+    batch_size = 10000
     # 定义模型结构
     net = LR(560871, 1)
-
     optimizer = tf.keras.optimizers.Adam()
-
     # 定义输入输出数据流
-    train_set = tf.data.TFRecordDataset(["../mtl/train.tfrecord"]).map(lambda record: parse_example(record)).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=yaml_config["batch_size"]))
-    test_set = tf.data.TFRecordDataset(["../mtl/test.tfrecord"]).map(lambda record: parse_example(record)).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=yaml_config["batch_size"]))
+    train_set = tf.data.TFRecordDataset(["./data/{}/train.tfrecord".format(camp)]).map(lambda record: parse_example(record)).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=batch_size))
+    test_set = tf.data.TFRecordDataset(["../data/{}/test.tfrecord".format(camp)]).map(lambda record: parse_example(record)).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=batch_size))
 
     train_epoch(net, train_set, optimizer)
     eval(net, test_set)  
